21575d9028f976593ff328ab46204ce93a3de043
Paul Cervenka <paul.cervenka@gmail.com>
2021-06-24 11:08:47 +0200

    chore: add prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { APP_ROUTES } from './app.routes';
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [AppComponent],
