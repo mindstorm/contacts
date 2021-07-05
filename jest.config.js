39cbd3e3af51ae0901a754c7b408cd66ee41c635
Paul Cervenka <paul.cervenka@gmail.com>
2021-07-05 07:47:28 +0200

    test: add coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,16 @@ module.exports = {
   moduleNameMapper: {},
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  coverageDirectory: '.reports',
+  collectCoverageFrom: ['src/app/**/*.(component|directive|pipe|service).ts'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
 };
