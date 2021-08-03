7a62365275933328cfd8fadd576d3926f37df228
Paul Cervenka <paul.cervenka@gmail.com>
2021-08-03 09:46:28 +0200

    feat: build specific module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 import { APP_ROUTES } from './app.routes';
+import { buildModules } from './build-specifics';
 import { CONTACTS_FEATURE_NAME, contactsReducer } from './store/contact.store';
 
 @NgModule({
@@ -16,7 +15,7 @@ import { CONTACTS_FEATURE_NAME, contactsReducer } from './store/contact.store';
     BrowserAnimationsModule,
     RouterModule.forRoot(APP_ROUTES),
     StoreModule.forRoot({ [CONTACTS_FEATURE_NAME]: contactsReducer }),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    buildModules, // Instrumentation must be imported after importing StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent],
